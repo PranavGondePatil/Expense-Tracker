a5b96e72623cedae683db4fa37df0d7e58d30c55
PranavGondePatil <92105994+PranavGondePatil@users.noreply.github.com>
2025-06-30 00:01:33 +0530

    Migrate from Create React App to Vite - Improved build performance and development experience

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import "./assets/css/index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
+// Use ReactDOM.render for React 16 compatibility
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
